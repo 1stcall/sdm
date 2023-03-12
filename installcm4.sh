4e2a890e8d569bca226c38075251ac2d501313d3
Carl McAlwane <carlmcalwane@hotmail.co.uk>
2023-03-12 11:59:07 +0000

    bookworm & proxmox

--- installcm4.sh
+++ installcm4.sh
@@ -3,8 +3,6 @@
 SECONDS=0
 ./customize.sh
 ./bf_rpicm4-1.sh
-./bf_rpi4b-1.sh
-./extfilesys.sh -v -v -o output/rpi4b-1-out.img /srv/netboot d21c0840
-mv -v /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img /mnt/rescuedata/$(date +'%Y-%M-%d')-rpicm4-1-lite.img.prev
+mv -v /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img.prev
 rsync --no-i-r --info=progress2 --verbose output/rpicm4-1-out.img /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img
 printf "Completed in %s mins.\n\n" $(awk -vx=${SECONDS} 'BEGIN{printf("%.2f\n",x/60)}')
