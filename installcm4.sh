20133b114b72f5b6e2ec9d8d8c7fdaa941180b75
Carl McAlwane <carlmcalwane@hotmail.co.uk>
2023-03-14 09:32:47 +0000

    Adding k3s (Kubenetes)

--- installcm4.sh
+++ installcm4.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 #
+OLDIFS=$IFS
 SECONDS=0
 ./customize.sh
+IFS=$OLDIFS
 ./bf_rpicm4-1.sh
-mv -v /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img.prev
-rsync --no-i-r --info=progress2 --verbose output/rpicm4-1-out.img /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img
+IFS=$OLDIFS
+#mv -v /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img.prev
+#rsync --no-i-r --info=progress2 --verbose output/rpicm4-1-out.img /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img
 printf "Completed in %s mins.\n\n" $(awk -vx=${SECONDS} 'BEGIN{printf("%.2f\n",x/60)}')
