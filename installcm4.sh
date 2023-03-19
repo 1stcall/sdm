a5e09ebcdecdc3e8fde29acfd684065ed4a216f1
Carl McAlwane <carlmcalwane@hotmail.co.uk>
2023-03-19 11:29:23 +0000

    code tidy up

--- installcm4.sh
+++ installcm4.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 #
-OLDIFS=$IFS
 SECONDS=0
-./customize.sh
-IFS=$OLDIFS
+BASEIMG=2023-03-14-debain_arm64-minimal.img ./customize.sh
 ./bf_rpicm4-1.sh
-IFS=$OLDIFS
-#mv -v /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img.prev
-#rsync --no-i-r --info=progress2 --verbose output/rpicm4-1-out.img /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img
+mv -v /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img.prev
+rsync --no-i-r --info=progress2 --verbose output/rpicm4-1-out.img /mnt/rescuedata/$(date +'%Y-%m-%d')-rpicm4-1-lite.img
 printf "Completed in %s mins.\n\n" $(awk -vx=${SECONDS} 'BEGIN{printf("%.2f\n",x/60)}')
